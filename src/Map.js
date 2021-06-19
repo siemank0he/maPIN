ca8f6c8529ed0cdb169fd2a13febe61229bc9ab3
siemank0he <rondeleczek88@gmail.com>
2021-06-20 01:54:32 +0200

    changes

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
+import MapboxWorker from 'worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker';
 
 mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;
 mapboxgl.accessToken='pk.eyJ1Ijoic2llbWFua29oZSIsImEiOiJja3Ezc2FvcWMwOGdhMnBxenZ1bWJzazJmIn0.3_qXAhQ6MZGcKUz1mNbNmg';
