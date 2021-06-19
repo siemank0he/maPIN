ffa85a91b57b1620ce9be60033f3f69fc2368b3b
siemank0he <rondeleczek88@gmail.com>
2021-06-20 01:25:46 +0200

    first commit

---
+++ src/Map.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken='pk.eyJ1Ijoic2llbWFua29oZSIsImEiOiJja3Ezc2FvcWMwOGdhMnBxenZ1bWJzazJmIn0.3_qXAhQ6MZGcKUz1mNbNmg';
+
+class Mapp extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            lng: 10,
+            lat: 80,
+            zoom: 4
+        }
+    }
+
+    componentDidMount() {
+        const map = new mapboxgl.Map({
+            container: this.mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [19.189587645337962, 47.820288049293275], 
+            zoom: this.state.zoom
+        })
+        
+    }
+
+    render(){
+        return(
+            <div>
+                <div ref={el => this.mapContainer = el} style={{width:'100%', height:'100vh'}}/>
+            </div>
+        )
+    }
+}
+
+export default Mapp;
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Mapa',
+        url: '#',
+        shortName: 'nav-links'
+    },
+    {
+        title: 'Services',
+        url: '#',
+        shortName: 'nav-links'
+    },
+    {
+        title: 'Products',
+        url: '#',
+        shortName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        shortName: 'nav-links'
+    },
+    {
+        title: 'Zaloguj się',
+        url: '#',
+        shortName: 'nav-links-mobile'
+    },
+]
